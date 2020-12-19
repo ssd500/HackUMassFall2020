08102ab638a3141281e7addd7528d45c41e458c5
Vani Korepu <43774862+vanikorepu@users.noreply.github.com>
2020-12-19 18:12:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -29,43 +29,120 @@ def get_top_tracks(sp, top_artists):
     return top_tracks_uri
 
 
-def get_mean_valence(sp, top_tracks_uri):
-    track_Valence = []
-    median_Valence = 0
-    stdev_Valence = 0
+def get_valence(sp, top_tracks_uri):
+
     for tracks in top_tracks_uri:
         tracks_all_data = sp.audio_features(tracks)
         for track_data in tracks_all_data:
             track_valence.append(track_data["valence"])
-        mean_Valence = statistics.mean(track_Valence)
-        stdev_Valence = statistics.stdev(track_Valence)
 
-    return mean_Valence, stdev_Valence
+def get_mean_Valence(track_Valence):
+    
+    return mean_Valence = statistics.mean(track_Valence)
 
-def get_mean_tempo(sp, top_tracks_uri):
-    track_Tempo = []
-    mean_Tempo = 0
-    stdev_Tempo = 0
+def get_stdev_Valence(track_Valence):
+    
+    return stdev_Valence = statistics.stdev(track_Valence)
+
+
+def get_tempo(sp, top_tracks_uri):
+    
     for tracks in top_tracks_uri:
         tracks_all_data = sp.audio_features(tracks)
-        for track_data in tracks_all_data:
+        for track_data in tracks_all_data
             track_Tempo.append(track_data["tempo"])
-        mean_Tempo = statistics.mean(track_Tempo)
-        stdev_Tempo = statistics.stdev(track_Tempo)
-    return mean_Tempo, stdev_Tempo
 
-def get_mean_energy(sp, top_tracks_uri):
-    track_Energy = []
-    mean_Energy = 0
-    stdev_Energy = 0
+def get_mean_Tempo(track_Tempo):      
+    
+    return mean_Tempo = statistics.mean(track_Tempo)
+
+def get_stdev_Tempo(track_Tempo):
+    
+    return stdev_Tempo = statistics.stdev(track_Tempo)
+
+
+def get_energy(sp, top_tracks_uri):
+    
+    for tracks in top_tracks_uri:
+        tracks_all_data = sp.audio_features(tracks)
+        for track_data in tracks_all_data
+            track_Energy.append(track_data["energy"])
+
+def get_mean_Energy(track_Energy):
+    
+    return mean_Energy = statistics.mean(track_Energy)
+
+def get_stdev_Energy(track_Energy):
+    
+    return stdev_Energy = statistics.stdev(track_Energy)
+
+# high_valence = all valence greater than equal to mean_valence + stddev_valence
+
+# medium_valence = all valence between mean_valence - stddev_valence & mean_valence + stddev_valence
+
+# low_valence = all valence lesser than mean_valence - stddev_valence
+
+# Mellow - Major, High valence, Low tempo, Low energy
+# Melancholy - Minor, low valence, Low tempo, Low energy
+
+# Happy - Major, high valence, medium tempo, medium energy
+# Sad - Minor, low valence, medium tempo, medium energy
+
+# Cheerful- Major, High valence, High tempo, High energy
+# Angry - Minor, low valence, High tempo, High energy
+
+
+def select_the_tracks(sp, top_tracks_uri, mood):
+    selected_tracks_uri = []
     for tracks in top_tracks_uri:
         tracks_all_data = sp.audio_features(tracks)
         for track_data in tracks_all_data:
-            track_Energy.append(track_data["energy"])
-        mean_Energy =  statistics.mean(track_Energy)
-        stdev_Energy = statistics.stdev(track_Energy)
-    return mean_Energy, stdev_Energy
+            try:
+                for mood in moods:
+                    if mood == "Mellow":
+                        if(track_data["mode"] == 1
+                        and track_data["valence"] >= (mean_Valence + stdev_Valence)
+                        and track_data["tempo"] < (mean_Tempo - stdev_Tempo)
+                        and track_data["energy"] < (mean_Energy - stdev_Energy))
+                            selected_tracks_uri.append(track_data["uri"])
+                    elif mood == "Melancholy":
+                        if(track_data["mode"] == 0
+                        and track_data["valence"] < (mean_Valence - stdev_Valence)
+                        and track_data["tempo"] < (mean_Tempo - stdev_Tempo)
+                        and track_data["energy"] < (mean_Energy - stdev_Energy))
+                            selected_tracks_uri.append(track_data["uri"])
+                    elif mood == "Happy":
+                        if(track_data["mode"] == 1
+                        and track_data["valence"] >= (mean_Valence + stdev_Valence)
+                        and ((mean_Tempo - stdev_Tempo) < track_data["tempo"] < (mean_Tempo + stdev_Tempo))
+                        and ((mean_Energy - stdev_Energy) < track_data["energy"] < (mean_Energy + stdev_Energy))
+                            selected_tracks_uri.append(track_data["uri"])
+                    elif mood == "Sad":
+                        if(track_data["mode"] == 0
+                        and track_data["valence"] < (mean_Valence - stdev_Valence)
+                        and ((mean_Tempo - stdev_Tempo) < track_data["tempo"] < (mean_Tempo + stdev_Tempo))
+                        and ((mean_Energy - stdev_Energy) < track_data["energy"] < (mean_Energy + stdev_Energy))
+                            selected_tracks_uri.append(track_data["uri"])
+                    elif mood == "Cheerful":
+                        if(track_data["mode"] == 1
+                        and track_data["valence"] >= (mean_Valence + stdev_Valence)
+                        and track_data["tempo"] >= (mean_Tempo + stdev_Tempo)
+                        and track_data["energy"] >= (mean_Energy + stdev_Energy))
+                            selected_tracks_uri.append(track_data["uri"])
+                    elif mood == "Angry":
+                        if(track_data["mode"] == 0
+                        and track_data["valence"] < (mean_Valence - stdev_Valence)
+                        and track_data["tempo"] >= (mean_Tempo + stdev_Tempo)
+                        and track_data["energy"] >= (mean_Energy + stdev_Energy))
+                            selected_tracks_uri.append(track_data["uri"])
+                except TypeError as type_e:
+                    ConnectionRefusedError
 
+    return selected_tracks_uri
+
+			   
+###
+        
 
 if feeling == 'happy':
 	if (valence lies between 0.8 and 1) and (danceability > 0.5) and (energy > 0.3):
