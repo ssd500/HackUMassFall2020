8ba94a27ce5e2c1c462e285eb33fcbbc0d9381b8
rohan-ynwa <70969245+rohan-ynwa@users.noreply.github.com>
2020-12-19 15:58:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 scope = "user-library-read user-top-read playlist-modify-public"
-ranges = ['short_term', 'medium_term', 'long_term']
 
 sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id = "326a452a53254fa9bc90368567c70861", client_secret = "7d518966cb804f019a4301db077c1c31", redirect_uri = "http://localhost:8888/callback", scope = scope))
 
